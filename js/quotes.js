6c186bd63bb0b719043c91a67288bb9d329153fb
b0060646@gmail.com <b0060646@gmail.com@5ce99331-199e-4641-f3b8-68a876e79a96>
2014-12-03 14:05:09 +0000

    1

---
+++ js/quotes.js
@@ -0,0 +1,29 @@
+﻿ 
+
+var quote = new Array(10);
+
+quote[0] = " గొప్ప  పనులు  పట్టుదల వల్ల పూర్తి అవుతాయి. కాని బలం వల్ల మాత్రం కాదు.” — <em>William Samuel Johnson</em>";
+
+quote[1] = " చిన్న పనులను నిర్లక్ష్యంగా చేసేవారు జీవితంలో పెద్ద విజయాలను సాధించలేరు..” — <em>Albert Einstein</em>";
+
+quote[2] = " ఖాళీ కడుపులతో వుంచడం సరైన రాజనీతి కాదు...” — <em>Albert Einstein</em>";
+
+quote[3] = " జ్ఞానికన్నా ఊహ గొప్పది..” — <em>Albert Einstein</em>";
+
+quote[4] = " అమలు చేయని శాసనాలు ప్రభుత్వానికి  ఎక్కువ హానికరం,అగౌరవం. ..” — <em>Albert Einstein</em>";
+
+quote[5] = " గొప్ప  వ్యక్తులకు సాధారణ వ్యక్తుల నుండి తీవ్ర వ్యతిరేకత సహజం...” — <em>Albert Einstein</em>";
+
+quote[6] = " ప్రకృతికి దగ్గరగా ఉంటే ...జీవిత సత్యాలు వాటంతటవే తెలుస్తాయి. .” — <em>Albert Einstein</em>";
+
+quote[7] = " హృదయంలో మాలిన్యంవున్న మనిషి ఆరోగ్యవంతంగా ఉండలేడు. .” — <em>Jawaharlal Nehru</em>";
+
+quote[8] = " పరాయివాళ్ల దివ్యభవనల్లో నివసించడం కన్నా సొంత కుటీరంలో ఉండటం మేలు .  ” — <em>Jawaharlal Nehru</em>";
+
+quote[9] = " అందరినీ ప్రేమించు, కొందరినే నమ్ము, ఎవరికీ  హాని తలపెట్టకు. ” — <em>William Shakespeare</em>";
+
+now=new Date()
+
+num=(now.getSeconds())%9
+
+document.write(quote[num])
