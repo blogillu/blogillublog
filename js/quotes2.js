2e23e897fff078476c8870e18a22238f34b0b01f
b0060646@gmail.com <b0060646@gmail.com@5ce99331-199e-4641-f3b8-68a876e79a96>
2014-12-03 14:16:54 +0000

    q

---
+++ js/quotes2.js
@@ -0,0 +1,29 @@
+﻿ 
+
+var quote = new Array(10);
+
+quote[0] = " బాలలు తల్లిదండ్రుల సంపద. .” — <em>William Shakespeare</em>";
+
+quote[1] = " చిన్న పనులను నిర్లక్ష్యంగా చేసేవారు జీవితంలో పెద్ద విజయాలను సాధించలేరు..” — <em>William Shakespeare</em>";
+
+quote[2] = " ఒక  సారి   నీ   నమ్మకాన్ని  వమ్ము  చేసిన వారిని ఇంకెన్నడూ నమ్మకు...” — <em>William Shakespeare</em>";
+
+quote[3] = " వివేక శూన్యుడైన మిత్రుడు , వివేకవంతుడైన శత్రువు కంటే ప్రమాదం...” — <em>William Shakespeare</em>";
+
+quote[4] = " మంచి చెడు అనేది ఏమి లేదు, ఆలోచనే అలా చేస్తుంది. ..” — <em>William Shakespeare</em>";
+
+quote[5] = " పుకారు అనేది ఊహలు,భావనలు,అసూయ కలిపి  ఉదాబడే పిల్లనగ్రోవి...” — <em>William Shakespeare</em>";
+
+quote[6] = " పసద్గుణాన్ని  మించిన సౌందర్యం లేదు. .” — <em>William Shakespeare</em>";
+
+quote[7] = " దొంగవాడు ధనవంతుని దోచుకొని జీవనం సాగిస్తున్నాడు..” — <em>William Shakespeare</em>";
+
+quote[8] = " తక్కువ కళతో  ఎక్కువ విషయాన్ని చెప్పాలి.     .  ” — <em>William Shakespeare</em>";
+
+quote[9] = " పిరికివాళ్ళు చావుకి ముందు ఎన్నోసార్లు చస్తూ బ్రతుకుతుంటారు, కానీ ధైర్యవంతుడు జీవితంలో ఒకే ఒక్కసారి చస్తాడు. . ” — <em>William Shakespeare</em>";
+
+now=new Date()
+
+num=(now.getSeconds())%9
+
+document.write(quote[num])
